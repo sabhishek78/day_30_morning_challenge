ad232b7baaccedf5a8df455c2c75b393316ca991
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-15 18:51:53 +0530

    finished. trying to refactor more.

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 // Challenge
 // Matryoshka dolls are traditionally wooden dolls that can be nested by fitting
 // smaller dolls into larger ones. Suppose arrays can be nested similarly,
@@ -13,5 +15,61 @@
 
 /// Create a function that returns true if every single sub-array inside an array
 /// can be nested Matroyshka style, and false otherwise.
+///
+matryoshka(List<List<int>> dolls) {
+  print(dolls);
+  dolls.forEach((element) => element.sort());
+  print("sorted List");
+  print(dolls);
+  List<List<int>> MaxMinList = findMaxMinOfEachSubList(dolls);
+}
+
+findMaxMinOfEachSubList(List<List<int>> dolls) {
+  List<List<int>> tempList = [];
+  dolls.forEach((e) {
+    List<int> temp = [];
+    temp.add(e[0]);
+    temp.add(e[e.length - 1]);
+    tempList.add(temp);
+  });
+  print(tempList); //prints each list with only the min and max element
+  // arrange list with first element in ascending order
+  int swap1;
+  int swap2;
+  for (int i = 0; i < tempList.length - 1; i++) {
+    for (int j = i + 1; j < tempList.length; j++) {
+      if (tempList[i][0] > tempList[j][0]) {
+        swap1 = tempList[j][0];
+        swap2 = tempList[j][1];
+        tempList[j][0] = tempList[i][0];
+        tempList[j][1] = tempList[i][1];
+        tempList[i][0] = swap1;
+        tempList[i][1] = swap2;
+      }
+    }
+  }
+  print(tempList);
+  print(checkForSecondElementDescending(tempList));
+}
+
+checkForSecondElementDescending(List<List<int>> tempList) {
+  for (int i = 0; i < tempList.length - 1; i++) {
+    if (!(tempList[i][1] > tempList[i + 1][1])) {
+      return false;
+    }
+  }
+  return true;
+}
+
 main() {
+  matryoshka([
+    [1, 10],
+    [2, 9],
+    [3, 8],
+    [7, 4],
+  ]);
+  matryoshka([
+    [2, 3, 9, 5],
+    [10, 2, 1]
+  ]);
 }
---
+++ bin/test.dart
@@ -0,0 +1,14 @@
+import 'main.dart';
+
+import 'package:test/test.dart';
+
+void main() {
+  test('Matryoshka', () {
+    expect(matryoshka([[1,10],[2,9],[3,8],[7,4],]),true);
+    expect(matryoshka([[2,3,9,5],[10,2,1]]),true);
+    expect(matryoshka([[2,3,9,5],[10,2,3]]),false);
+    expect(matryoshka([[2,3,9,5],[2,3,9,5]]),false);
+    expect(matryoshka([[2],[3]]),false);
+  });
+
+}
